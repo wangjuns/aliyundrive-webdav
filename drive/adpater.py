8ca5f81dca8da0566b240d5ec33a736b00f74b9e
wangjun <jun.wangj@me.com>
2022-12-04 18:37:27 +0800

    使用dict实现缓存

--- drive/adpater.py
+++ drive/adpater.py
@@ -1,8 +1,10 @@
+import atexit
+import json
 import logging
 import os
 import shelve
 import threading
-from typing import List
+from typing import List, Optional
 
 from wsgidav import util
 
@@ -14,13 +16,14 @@ logger = logging.getLogger('aliyundrive-dav')
 cache_dir = os.path.join(ROOT_DIR, 'cache')
 cache_path = os.path.join(cache_dir, 'data')
 
+
 class AliyunDriveAdapter():
 
     def __init__(self) -> None:
         self.drive = AliyunDrive()
         self.lock = threading.Lock()
-        # self._load_cache()
-        # atexit.register(self.close)
+        self._load_cache()
+        atexit.register(self.close)
 
     def _load_cache(self):
         """
@@ -29,7 +32,7 @@ class AliyunDriveAdapter():
         if not os.path.exists(cache_dir):
             os.mkdir(cache_dir)
 
-        self.cache = shelve.open(cache_path)
+        self.cache = {}
 
     def close(self):
         """
@@ -37,19 +40,28 @@ class AliyunDriveAdapter():
         """
         self.cache.close()
 
+    def _key(self, key: str, group: Optional[str] = None):
+        return f'{group}.{key}' if group else key
+
+    def readCache(self, key: str, group: Optional[str] = None):
+        with self.lock:
+            return self.cache.get(self._key(key, group))
+
+    def putCache(self, key: str, value, group: Optional[str] = None):
+        with self.lock:
+            self.cache.update({self._key(key, group): value})
 
     def get_item_by_path(self, path: str):
         """
         根据路径获取file_item
         """
         assert path.startswith('/') and path != '/'
-        uri_parent = util.get_uri_parent(path)
-        uri_name = util.get_uri_name(path)
-        for item in self.get_file_list_by_path(uri_parent):
-            if item.name == uri_name:
-                return item
 
-        return None
+        file_item = self.readCache(path, "file_item")
+        if file_item is not None:
+            return file_item
+
+        return self.get_file_id(path)
 
     def get_file_list_by_path(self, path: str) -> List[FileItem]:
         """
@@ -61,14 +73,40 @@ class AliyunDriveAdapter():
         if uri_parent is None:
             return self._get_file_list('/', 'root')
 
-        uri_name = util.get_uri_name(path)
-        for item in self.get_file_list_by_path(uri_parent):
-            if item.name == uri_name:
-                return self._get_file_list(path, item.file_id)
+        item = self.readCache(path.rstrip("/"), "file_item")
+        if item is None:
+            item = self.get_file_id(path)
+
+        if item is not None:
+            return self._get_file_list(path, item.file_id)
 
         return []
 
-    def _get_file_list(self, path: str, file_id: str):
+    def get_file_id(self, path: str) -> FileItem:
+        file_item = self.readCache(path, "file_item")
+        if file_item is not None:
+            return file_item
+
+        parents = path.rstrip('/').split('/')
+        parents = parents[:-1]
+
+        file_items = []
+        full_path = ""
+        for parent in parents:
+            if parent == '':
+                file_items = self._get_file_list('/', 'root')
+            else:
+                t_path = "%s/%s" % (full_path, parent)
+                item: FileItem = next(x for x in file_items if x.name == parent)
+                file_items = self._get_file_list(t_path, item.file_id)
+                full_path = t_path
+
+            for item in file_items:
+                self.putCache("%s/%s" % (full_path, item.name), item, "file_item")
+
+        return self.readCache(path.rstrip('/'), "file_item")
+
+    def _get_file_list(self, path: str, file_id: str) -> List[FileItem]:
         file_list = []
 
         for item in self.drive.list_all_files(file_id):
